864725b6b19a7c065e4c704cb6a009ae327fd2e9
kuliodev <sobegogo@gmail.com>
2017-10-05 02:49:02 +0000

    call to phone link

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,12 @@
 			<div id="footer" class="cf">
 			
 				<div class="column three">
-					<strong>Phone</strong>
-					889-979-9999
+					<strong>Phone.:</strong>
+					<a href="tel://+12345678912" style="text-decoration:none;  color: #000" class="not-active">(234) 567-8912</a>
 				</div><!-- column -->
 				
 				<div class="column three">
-					<strong>Location</strong>
+					<strong>Location.:</strong>
 				20314 NW Roy Rogers Road Suite #535<br>
 					Sherwood, NM 98140
 				</div><!-- column -->
@@ -14,7 +14,7 @@
 				<div class="column three last">
 					
 				
-					<strong>Hours</strong>
+					<strong>Hours.:</strong>
 					<em>Tuesday - Thursday</em><br>
 					1:00pm - 9:00pm<br><br>
 					
@@ -32,7 +32,7 @@
 			
 			</div><!-- footer -->
 			
-			<small>&copy;<?php echo date('Y'); ?> <a href="http://kulio712.com/" target="_blank"> <?php echo $companyName; ?></a></small>
+			<small>&copy;<?php echo date('Y'); ?> <a href="http://kulio712.com/" target="_blank" style="text-decoration:none; color: #000" > <?php echo $companyName; ?></a></small>
 		
 		</div><!-- content -->
 		
